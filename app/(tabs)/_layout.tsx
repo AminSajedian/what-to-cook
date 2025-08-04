20e9b8fa130f509a71cf601aa178ead47e51f61c
Amin Sajedian <aminsajedian@gmail.com>
2025-08-04 22:43:29 +0100

    refactor: standardize import statements and improve layout properties in Tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,13 +1,13 @@
-import { HapticTab } from '@/components/HapticTab';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { StoreProvider } from '@/contexts/StoreContext';
-import { useColorScheme } from '@/hooks/useColorScheme';
-import MaterialIcons from '@expo/vector-icons/MaterialIcons';
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { HapticTab } from "@/components/HapticTab";
+import TabBarBackground from "@/components/ui/TabBarBackground";
+import { Colors } from "@/constants/Colors";
+import { StoreProvider } from "@/contexts/StoreContext";
+import { useColorScheme } from "@/hooks/useColorScheme";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import { Tabs } from "expo-router";
+import React from "react";
+import { Platform } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function Layout() {
   const colorScheme = useColorScheme();
@@ -17,36 +17,48 @@ export default function Layout() {
       <StoreProvider>
         <Tabs
           screenOptions={{
-            tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+            // Remove extra space above header on Android
+            headerStatusBarHeight: Platform.OS === "android" ? 0 : undefined,
+            tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
             tabBarButton: HapticTab,
             tabBarBackground: TabBarBackground,
             tabBarStyle: Platform.select({
               ios: {
-                position: 'absolute',
+                position: "absolute",
               },
               default: {},
             }),
             headerShown: true,
             headerStyle: {
-              backgroundColor: Colors[colorScheme ?? 'light'].background,
+              backgroundColor: Colors[colorScheme ?? "light"].background,
               borderBottomWidth: 0,
               elevation: 0,
               shadowOpacity: 0,
               minHeight: 60, // Only supported properties
             },
             headerTitleStyle: {
-              fontWeight: 'bold',
+              fontWeight: "bold",
               fontSize: 22,
               letterSpacing: 0.2,
-              color: Colors[colorScheme ?? 'light'].text,
+              color: Colors[colorScheme ?? "light"].text,
             },
-            headerTitleAlign: 'center',
+            headerTitleAlign: "center",
+            tabBarIconStyle: {
+              marginTop: 3, // Adjust icon position
+              marginBottom: 3, // Adjust icon position
+            },
+            tabBarLabelStyle: {
+              fontSize: 12,
+              fontWeight: "500",
+              letterSpacing: 0.1,
+              marginBottom: Platform.OS === "ios" ? -2 : 0, // Adjust label position
+            },
           }}
         >
           <Tabs.Screen
             name="index"
             options={{
-              title: 'Week Plan',
+              title: "Week Plan",
               tabBarIcon: ({ color }) => (
                 <MaterialIcons size={28} name="home" color={color} />
               ),
@@ -55,7 +67,7 @@ export default function Layout() {
           <Tabs.Screen
             name="food_list"
             options={{
-              title: 'Food List',
+              title: "Food List",
               tabBarIcon: ({ color }) => (
                 <MaterialIcons size={28} name="restaurant" color={color} />
               ),
@@ -64,7 +76,7 @@ export default function Layout() {
           <Tabs.Screen
             name="settings"
             options={{
-              title: 'Settings',
+              title: "Settings",
               tabBarIcon: ({ color }) => (
                 <MaterialIcons size={28} name="settings" color={color} />
               ),

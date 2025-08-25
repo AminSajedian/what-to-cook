cdd14e2f3bdf75bed6a5fa953812316c8e1c60af
Amin Sajedian <aminsajedian@gmail.com>
2025-08-25 20:15:13 +0100

    refactor: remove StoreProvider and streamline Tabs component structure in Layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,7 +1,6 @@
 import { HapticTab } from "@/components/HapticTab";
 import TabBarBackground from "@/components/ui/TabBarBackground";
 import { Colors } from "@/constants/Colors";
-import { StoreProvider } from "@/contexts/StoreContext";
 import { useColorScheme } from "@/hooks/useColorScheme";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import { Tabs } from "expo-router";
@@ -14,76 +13,74 @@ export default function Layout() {
 
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <StoreProvider>
-        <Tabs
-          screenOptions={{
-            // Remove extra space above header on Android
-            headerStatusBarHeight: Platform.OS === "android" ? 0 : undefined,
-            tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
-            tabBarButton: HapticTab,
-            tabBarBackground: TabBarBackground,
-            tabBarStyle: Platform.select({
-              ios: {
-                position: "absolute",
-              },
-              default: {},
-            }),
-            headerShown: true,
-            headerStyle: {
-              backgroundColor: Colors[colorScheme ?? "light"].background,
-              borderBottomWidth: 0,
-              elevation: 0,
-              shadowOpacity: 0,
-              minHeight: 60, // Only supported properties
+      <Tabs
+        screenOptions={{
+          // Remove extra space above header on Android
+          headerStatusBarHeight: Platform.OS === "android" ? 0 : undefined,
+          tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+          tabBarButton: HapticTab,
+          tabBarBackground: TabBarBackground,
+          tabBarStyle: Platform.select({
+            ios: {
+              position: "absolute",
             },
-            headerTitleStyle: {
-              fontWeight: "bold",
-              fontSize: 22,
-              letterSpacing: 0.2,
-              color: Colors[colorScheme ?? "light"].text,
-            },
-            headerTitleAlign: "center",
-            tabBarIconStyle: {
-              marginTop: 3, // Adjust icon position
-              marginBottom: 3, // Adjust icon position
-            },
-            tabBarLabelStyle: {
-              fontSize: 12,
-              fontWeight: "500",
-              letterSpacing: 0.1,
-              marginBottom: Platform.OS === "ios" ? -2 : 0, // Adjust label position
-            },
+            default: {},
+          }),
+          headerShown: true,
+          headerStyle: {
+            backgroundColor: Colors[colorScheme ?? "light"].background,
+            borderBottomWidth: 0,
+            elevation: 0,
+            shadowOpacity: 0,
+            minHeight: 60, // Only supported properties
+          },
+          headerTitleStyle: {
+            fontWeight: "bold",
+            fontSize: 22,
+            letterSpacing: 0.2,
+            color: Colors[colorScheme ?? "light"].text,
+          },
+          headerTitleAlign: "center",
+          tabBarIconStyle: {
+            marginTop: 3, // Adjust icon position
+            marginBottom: 3, // Adjust icon position
+          },
+          tabBarLabelStyle: {
+            fontSize: 12,
+            fontWeight: "500",
+            letterSpacing: 0.1,
+            marginBottom: Platform.OS === "ios" ? -2 : 0, // Adjust label position
+          },
+        }}
+      >
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: "Week Plan",
+            tabBarIcon: ({ color }) => (
+              <MaterialIcons size={28} name="home" color={color} />
+            ),
           }}
-        >
-          <Tabs.Screen
-            name="index"
-            options={{
-              title: "Week Plan",
-              tabBarIcon: ({ color }) => (
-                <MaterialIcons size={28} name="home" color={color} />
-              ),
-            }}
-          />
-          <Tabs.Screen
-            name="food_list"
-            options={{
-              title: "Food List",
-              tabBarIcon: ({ color }) => (
-                <MaterialIcons size={28} name="restaurant" color={color} />
-              ),
-            }}
-          />
-          <Tabs.Screen
-            name="settings"
-            options={{
-              title: "Settings",
-              tabBarIcon: ({ color }) => (
-                <MaterialIcons size={28} name="settings" color={color} />
-              ),
-            }}
-          />
-        </Tabs>
-      </StoreProvider>
+        />
+        <Tabs.Screen
+          name="food_list"
+          options={{
+            title: "Food List",
+            tabBarIcon: ({ color }) => (
+              <MaterialIcons size={28} name="restaurant" color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="settings"
+          options={{
+            title: "Settings",
+            tabBarIcon: ({ color }) => (
+              <MaterialIcons size={28} name="settings" color={color} />
+            ),
+          }}
+        />
+      </Tabs>
     </GestureHandlerRootView>
   );
 }
